b254522a08cdb85eebc78d161a1c064e31034c81
truejojo <johannesveh@yahoo.de>
2023-02-17 12:51:49 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+const App = () => (
+  <div>
+    <h1 className="text-center uppercase clr-accent-400 p-block-40 bg-neutral-900 fw-bold fs-900">Hello World!</h1>
+  </div>
+);
+
+export default App;
